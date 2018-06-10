0289c07faae2798da1f92e31ea347e71dcfa4cf8
pablohenrique <pablo@softbox.com.br>
2018-06-10 14:16:07 -0300

    fix(Core): fixing functions

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import Input from '../components/Input'
+import TextArea from '../components/TextArea'
+import Message from '../components/Message'
 
 class App extends Component {
   render() {
@@ -10,6 +13,18 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+
+        <Input
+          label="vaca" />
+        <TextArea
+          placeholder="vaca" />
+
+        <Message
+          right
+          author="You"
+          message="akljsdkl aksjd kalsj dadj aksdlajs djaskd akjd laj sdla sdlj asjd asdjasjd aksjdklas dka skd alskjd aklsjd "
+          timestamp={new Date().toDateString()}/>
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- src/utils/util.js
+++ src/utils/util.js
@@ -1,5 +1,1 @@
-import { curry } from 'ramda'
-
-const funcAppendClass = (appendedClassName, classNameProp) => [appendedClassName, classNameProp || ''].join(' ').trim()
-
-export const appendClass = curry(funcAppendClass)
+export const appendClass = (...classes) => classes.join(' ').trim()
