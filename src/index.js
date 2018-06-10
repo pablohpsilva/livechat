f08efb3d8602b7ad32d182bdb2105eca8d8035b7
pablohenrique <pablo@softbox.com.br>
2018-06-10 09:57:29 -0300

    fix(Core): ejected project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/reset.scss';
+import './styles/main.scss';
+import App from './pages/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
