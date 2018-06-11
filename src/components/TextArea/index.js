731837f9a48e778199929503afffeae1393a4188
pablohenrique <pablo@softbox.com.br>
2018-06-10 22:38:57 -0300

    comp(TextArea): enhanced TextArea component

--- src/components/TextArea/index.js
+++ src/components/TextArea/index.js
@@ -1,17 +1,48 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { omit, evolve } from 'ramda'
 import { appendClass } from '../../utils/util'
 import './TextArea.scss'
 
 class TextArea extends React.Component {
-  onChangeEditableDiv (onChange) {
-    onChange(this.refs.contentEditableDiv.textContent)
+  constructor(props) {
+    super(props)
+
+    this.onEnter = this.props.onEnter
+    this.state = {
+      text: 'vaca'
+    }
+
+    this.onChangeText = this.onChangeText.bind(this)
+    this.cleanText = this.cleanText.bind(this)
+  }
+
+  cleanText() {
+    this.setState(state => Object.assign(state, { text: '' }))
+  }
+
+  onChangeText({ target: { value } }) {
+    this.setState(state => Object.assign(state, { text: value }))
+  }
+
+  componentDidMount() {
+    this.refs.textarea.addEventListener('keydown', (e) => {
+      if ((this.props.submitOnEnter && e.keyCode === 13 && !e.metaKey)||
+        (!this.props.submitOnEnter && e.keyCode === 13 && e.metaKey)) {
+        this.onEnter(this.state.text)
+        this.cleanText()
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.refs.textarea.removeEventListener('keydown', () => ({}))
   }
 
   render () {
-    const {
-      className
-    } = this.props
+    const { className } = this.props
+    const textAreaProps = omit(['onEnter', 'submitOnEnter'], this.props)
+
     const wrapperComputedClass = appendClass('TextAreaWrapper', className)
     const textAreaComputedClass = appendClass('textarea-text', className)
 
@@ -19,7 +50,10 @@ class TextArea extends React.Component {
       <div
         className={wrapperComputedClass}>
         <textarea
-          {...this.props}
+          ref="textarea"
+          {...textAreaProps}
+          onChange={this.onChangeText}
+          value={this.state.text}
           className={textAreaComputedClass}>
         </textarea>
       </div>
@@ -38,7 +72,9 @@ TextArea.propTypes = {
   disabled: PropTypes.bool,
   rows: PropTypes.number,
   cols: PropTypes.number,
-  onChange: PropTypes.func,
+  submitOnEnter: PropTypes.bool,
+  // onChange: PropTypes.func,
+  onEnter: PropTypes.func.isRequired
 }
 
 export default TextArea
