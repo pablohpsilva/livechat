2861065d4f5cac05863fcc45df361a5927370876
pablohenrique <pablo@softbox.com.br>
2018-06-11 10:04:56 -0300

    comp(Settings): added flux on Settings page

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -1,53 +1,21 @@
 import React from 'react'
-import { appendClass } from '../../utils/util'
-import { curry } from 'ramda'
+import { connect } from 'react-redux'
+import {
+  updateInterface,
+  updateClockFormat,
+  updateCtrlEnter,
+  resetConfig,
+  saveUsername
+} from '../../store/actions';
 
 import Input from '../../components/Input'
 import RadioOptions from '../../components/RadioOptions'
+import { appendClass } from '../../utils/util'
 import './Settings.scss'
 
 class Settings extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      username: 'Guest-World-1',
-      interfaceColor: 'light',
-      clockDisplay: '12',
-      ctrlEnter: 'off',
-      defaultConfig: {
-        username: 'Guest-World-1',
-        interfaceColor: 'light',
-        clockDisplay: '12',
-        ctrlEnter: 'off'
-      }
-    }
-
-    this.onChangeInputText = curry(this.onChangeInputText.bind(this))
-    this.onChangeRadio = curry(this.onChangeRadio.bind(this))
-    this.onClickDefault = curry(this.onClickDefault.bind(this))
-  }
-
-  setNewState(newState) {
-    this.setState((state) => Object.assign(state, newState))
-  }
-
-  onChangeInputText(key, { target: { value } }) {
-    const data = { [key]: value || `Guest-${Math.random().toString(36).slice(4)}` }
-    this.setNewState(data)
-  }
-
-  onChangeRadio(key, { target: { value } }) {
-    const data = { [key]: value }
-    this.setNewState(data)
-  }
-
-  onClickDefault() {
-    this.setNewState(this.state.defaultConfig)
-  }
-
   setTheme() {
-    const darkTheme = (this.state.interfaceColor === 'dark')
+    const darkTheme = (this.props.interfaceColor === 'dark')
     document.documentElement.setAttribute('data-theme', darkTheme ? 'dark' : 'light')
   }
 
@@ -55,17 +23,24 @@ class Settings extends React.Component {
     this.setTheme()
   }
 
-  componentWillUpdate() {
+  componentDidUpdate() {
     this.setTheme()
   }
 
   render () {
+
     const {
       username,
       interfaceColor,
       clockDisplay,
       ctrlEnter,
-    } = this.state
+      updateInterface,
+      updateClockFormat,
+      updateCtrlEnter,
+      resetConfig,
+      saveUsername
+    } = this.props
+
     const wrapperComputedClass = appendClass('settings-wrapper', this.props.className)
 
     return (
@@ -75,13 +50,13 @@ class Settings extends React.Component {
           className="settings-config">
           <Input
             label="username"
-            onChange={this.onChangeInputText('username')}
+            onChange={({ target: { value } }) => saveUsername(value)}
             value={username} />
 
           <RadioOptions
             label="Interface color"
             checked={interfaceColor}
-            onChange={this.onChangeRadio('interfaceColor')}
+            onChange={({ target: { value } }) => updateInterface(value)}
             options={[
               { value: 'light', text: 'Light' },
               { value: 'dark', text: 'Dark' },
@@ -90,7 +65,7 @@ class Settings extends React.Component {
           <RadioOptions
             label="Clock display"
             checked={clockDisplay}
-            onChange={this.onChangeRadio('clockDisplay')}
+            onChange={({ target: { value } }) => updateClockFormat(value)}
             options={[
               { value: '12', text: '12 hours' },
               { value: '24', text: '24 hours' },
@@ -99,7 +74,7 @@ class Settings extends React.Component {
           <RadioOptions
             label="Ctrl+Enter to send Messages"
             checked={ctrlEnter}
-            onChange={this.onChangeRadio('ctrlEnter')}
+            onChange={({ target: { value } }) => updateCtrlEnter(value)}
             options={[
               { value: 'on', text: 'On' },
               { value: 'off', text: 'Off' },
@@ -115,7 +90,7 @@ class Settings extends React.Component {
         <div
           className="settings-footer">
           <button
-            onClick={this.onClickDefault}
+            onClick={resetConfig}
             className="settings-button-restore">
             Restore to defaults
           </button>
@@ -125,4 +100,22 @@ class Settings extends React.Component {
   }
 }
 
-export default Settings
+const mapStateToProps = store => ({
+  interfaceColor: store.settingsState.interfaceColor,
+  clockDisplay: store.settingsState.clockDisplay,
+  ctrlEnter: store.settingsState.ctrlEnter,
+  username: store.userState.username,
+})
+
+const mapDispatchToProps = dispatch => ({
+  updateInterface: (value) => dispatch(updateInterface(value)),
+  updateClockFormat: (value) => dispatch(updateClockFormat(value)),
+  updateCtrlEnter: (value) => dispatch(updateCtrlEnter(value)),
+  resetConfig: () => dispatch(resetConfig()),
+  saveUsername: (value) => dispatch(saveUsername(value)),
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Settings)
