f9f282a2a0bb5246a080370d6ebf01c213d62495
pablohenrique <pablo@softbox.com.br>
2018-06-11 10:04:28 -0300

    core(Root): installed Redux

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './static/styles/reset.scss';
 import './static/styles/main.scss';
 import App from './pages/App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { Store } from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider
+    store={Store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
